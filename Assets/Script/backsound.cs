d7f05cc21d3f86e699c96bff315fa21daf850005
DendyRamdhani <dendiepie3@gmail.com>
2021-09-12 14:58:43 +0700

    File project

---
+++ Assets/Script/backsound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backsound : MonoBehaviour
+{
+    private static backsound bgsound;
+
+     void Awake()
+    {
+        if(bgsound == null)
+        {
+            bgsound = this;
+            DontDestroyOnLoad(bgsound);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
